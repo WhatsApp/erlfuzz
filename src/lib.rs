e6089512f37cb3bcc1a04908f8a49db2d91b27c0
Robin Morisset <rmorisset@meta.com>
2023-06-01 02:35:02 -0700

    Remove `iter_zip` feature
    Summary:
    There was a warning since I updated my installation of Rust:
    > warning: the feature `iter_zip` has been stable since 1.59.0 and no longer requires an attribute to enable
    
    Reviewed By: TD5
    
    Differential Revision: D46273744
    
    fbshipit-source-id: 6ddf08f4b5f5db2c6085485dd6ee470bca58f1bf

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@
  * This source code is licensed under the Apache 2.0 license found in
  * the LICENSE file in the root directory of this source tree.
  */
-#![feature(iter_zip)]
 
 mod ast;
 mod context;
